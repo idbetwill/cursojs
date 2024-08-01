cc9e248cbf30823f60ca46ea419f17393d8e8292
idbetwill <idbetwill@users.noreply.github.com>
2024-08-01 12:32:54 -0500

    Correcciones Tuto Ejercicio 5

--- js/basicos-ejercicios.js
+++ js/basicos-ejercicios.js
@@ -133,14 +133,24 @@ repeatText("Hola Mundo");
 
 // Hay diferentes formas de invertir un texto en JS https://www.freecodecamp.org/espanol/news/tres-formas-de-invertir-una-cadena-en-javascript/
 
-/* const invest = (investText) => {
+const invest = (investText) => {
   let invested = "";
   for (let i = investText.length - 1; i >= 0; i--) {
     invested += investText[i];
   }
   return invested;
 };
-console.info(invest("Hola")); */
+console.info(invest("Hola"));
+
+// **********     Solución tutor ejercicio 5     **********
+
+const invest2 = (investText2) =>
+  !investText2
+    ? console.warn("No ingresaste una cadena de texto")
+    : console.info(investText2.split("").reverse().join(""));
+
+invest2("Hola Mundo");
+invest2();
 
 // **********     Ejercicio 6     **********
 
@@ -175,3 +185,7 @@ const delWord = (string = "") =>
     : console.info(string.replace(/,/g, ""));
 
 delWord("Hola, voy, a elimnar, las, comas");
+
+/*9) Programa una función que obtenga un numero aleatorio entre 501 y 600.
+10) Programa una función que reciba un número y evalúe si es capicúa o no (que se lee igual en un sentido que en otro), pe. miFuncion(2002) devolverá true.
+11) Programa una función que calcule el factorial de un número (El factorial de un entero positivo n, se define como el producto de todos los números enteros positivos desde 1 hasta n), pe. miFuncion(5) devolverá 120.*/
