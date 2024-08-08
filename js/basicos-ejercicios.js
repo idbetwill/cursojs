d269cf32371361ff6ab81a6964e9a13a872758bd
idbetwill <idbetwill@users.noreply.github.com>
2024-08-08 00:00:06 -0500

    Write Excersices (6/10)

--- js/basicos-ejercicios.js
+++ js/basicos-ejercicios.js
@@ -463,4 +463,6 @@ const calDate = (year = undefined, month = undefined, day = undefined) => {
 
 calDate(1999, 7, 17);
 
-/**/
+/*18) Programa una función que dada una cadena de texto cuente el número de vocales y consonantes, pe. miFuncion("Hola Mundo") devuelva Vocales: 4, Consonantes: 5.
+19) Programa una función que valide que un texto sea un nombre válido, pe. miFuncion("Jonathan MirCha") devolverá verdadero.
+20) Programa una función que valide que un texto sea un email válido, pe. miFuncion("jonmircha@gmail.com") devolverá verdadero.*/
